f03fd856cb86561d1fc5be3a65f6689b53a6f8ed
jalal <jalal@nulab.co.jp>
2017-06-16 14:44:53 +0900

    Fix backlog api path

--- src/main/scala/com/github/chaabaj/openid/apis/backlog/BacklogOAuthClient.scala
+++ src/main/scala/com/github/chaabaj/openid/apis/backlog/BacklogOAuthClient.scala
@@ -21,12 +21,12 @@ trait BacklogOAuthClient
 private class BacklogOAuthClientImpl(backlogUri: Uri)(implicit actorSystem: ActorSystem, timeout: FiniteDuration)
   extends BacklogOAuthClient {
   override val httpClient: HttpClient = HttpClient()
-  override protected def accessTokenUrl: String = backlogUri.withPath(Path("api/v2/oauth2/token")).toString()
+  override protected def accessTokenUrl: String = backlogUri.withPath(Path("/api/v2/oauth2/token")).toString()
 
   override def getUserInfo(token: AccessTokenSuccess)(implicit exc: ExecutionContext): Future[UserInfo] = {
     // TODO validate id_token
     val httpRequest = HttpRequest(
-      uri = backlogUri.withPath(Path("api/v2/users/myself"))
+      uri = backlogUri.withPath(Path("/api/v2/users/myself"))
     ).withHeaders(Authorization(OAuth2BearerToken(token.accessToken)))
     httpClient.request(httpRequest)
       .map(_.convertTo[UserInfo])
